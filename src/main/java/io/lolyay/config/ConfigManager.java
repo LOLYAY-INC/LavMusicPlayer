3586e9381d3078a0b25a1b479f2b9ace660bb475
LOLYAY <lolyay@lolyay.de>
2025-06-15 15:46:47 +0200

    Make Config Robust

--- src/main/java/io/lolyay/config/ConfigManager.java
+++ src/main/java/io/lolyay/config/ConfigManager.java
@@ -11,16 +11,28 @@ public class ConfigManager {
 
     public static String getConfig(String key) {
         String conf = configMap.get(key).toString();
+        if (configMap.get(key) == null) {
+            Logger.err("Config file is not valid or is empty, It may be out of date, please update it so it includes: " + key);
+            System.exit(1);
+        }
         if (conf.equals("${project.version}"))
             return "A development build"; // if it isnt a gh release
         return configMap.get(key).toString();
     }
 
     public static boolean getConfigBool(String key) {
+        if (configMap.get(key) == null) {
+            Logger.err("Config file is not valid or is empty, It may be out of date, please update it so it includes: " + key);
+            System.exit(1);
+        }
         return (boolean) configMap.get(key);
     }
 
     public static Object getConfigRaw(String key) {
+        if (configMap.get(key) == null) {
+            Logger.err("Config file is not valid or is empty, It may be out of date, please update it so it includes: " + key);
+            System.exit(1);
+        }
         return configMap.get(key);
     }
 
