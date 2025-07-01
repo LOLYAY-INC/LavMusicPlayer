f00646c456b5a1ce915d514b45cd85faf5d68cfd
LOLYAY <lolyay@lolyay.de>
2025-07-01 20:00:29 +0200

    Yep last commit errors

---
+++ src/main/java/io/lolyay/events/OnTrackEnd.java
@@ -0,0 +1,24 @@
+package io.lolyay.events;
+
+import dev.arbjerg.lavalink.client.event.TrackEndEvent;
+import io.lolyay.JdaMain;
+import io.lolyay.customevents.EventListener;
+import io.lolyay.customevents.events.music.TrackEndedEvent;
+import io.lolyay.musicbot.GuildMusicManager;
+import io.lolyay.utils.Logger;
+
+public class OnTrackEnd {
+    @EventListener
+    public void onTrackEnd(TrackEndEvent event) {
+        Logger.debug("TrackEndEvent for guild " + event.getGuildId());
+        GuildMusicManager musicManager = JdaMain.playerManager.getGuildMusicManager(event.getGuildId());
+
+        if (musicManager.getQueManager().isEmpty()) return;
+
+        JdaMain.eventBus.post(new TrackEndedEvent(musicManager.getQueue().getFirst(), event.getEndReason(), event.getGuildId(), event.getNode()));
+
+        if (event.getEndReason().getMayStartNext()) {
+            musicManager.onTrackEnd();
+        }
+    }
+}
