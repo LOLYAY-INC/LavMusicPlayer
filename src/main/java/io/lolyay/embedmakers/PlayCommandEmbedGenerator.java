1788711554539d2d9baafc35c384789524a84fd1
LOLYAY <github@lolyay.de>
2025-07-03 14:08:27 +0200

    Create PlayCommandEmbedGenerator.java

---
+++ src/main/java/io/lolyay/embedmakers/PlayCommandEmbedGenerator.java
@@ -0,0 +1,22 @@
+package io.lolyay.embedmakers;
+
+import io.lolyay.JdaMain;
+import io.lolyay.config.ConfigManager;
+import io.lolyay.config.guildconfig.GuildConfig;
+import io.lolyay.config.guildconfig.GuildConfigManager;
+import io.lolyay.musicbot.GuildMusicManager;
+import Io.lolyay.musicbot.tracks.MusicAudioTrack;
+import io.lolyay.musicbot.backendswapper.client.ClientPlayerManager;
+import io.lolyay.musicbot.backendswapper.structs.ENVIRONMENT;
+import net.dv8tion.jda.api.EmbedBuilder;
+
+import java.awt.*;
+
+public class StatusEmbedGenerator {
+    public static EmbedBuilder generate(MusicAudioTrack track) {
+        EmbedBuilder builder = new EmbedBuilder();
+       
+        return builder;
+    }
+
+}
