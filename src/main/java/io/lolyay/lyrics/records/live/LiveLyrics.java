d189db73d392a9c14e87e905c06ef8f5e89f56b5
LOLYAY <lolyay@lolyay.de>
2025-07-12 11:51:14 +0200

    Updated and move lyrics to LyricsV2

--- src/main/java/io/lolyay/lyrics/records/live/LiveLyrics.java
+++
@@ -1,6 +0,0 @@
-package io.lolyay.lyrics.records.live;
-
-import io.lolyay.lyrics.records.SearchLyrics;
-
-public record LiveLyrics(SearchLyrics query, String content, String source, LiveData liveData) {
-}
--- src/main/java/io/lolyay/panel/packet/packets/C2S/lyrics/C2SGetLyricsPacket.java
+++ src/main/java/io/lolyay/panel/packet/packets/C2S/lyrics/C2SGetLyricsPacket.java
@@ -1,12 +1,16 @@
 package io.lolyay.panel.packet.packets.C2S.lyrics;
 
 import com.google.gson.annotations.Expose;
+import io.lolyay.LavMusicPlayer;
+import io.lolyay.lyrics.LyricsNotFoundException;
 import io.lolyay.lyrics.getters.LyricsGetterManager;
 import io.lolyay.music.track.MusicAudioTrack;
 import io.lolyay.panel.packet.AbstractPacket;
 import io.lolyay.panel.packet.C2SPacket;
 import io.lolyay.panel.packet.handlers.PacketHandler;
 import io.lolyay.panel.packet.packets.S2C.lyrics.S2CGetLyricsPacket;
+import io.lolyay.panel.packet.packets.S2C.utilpackets.S2CErrorPacket;
+import io.lolyay.utils.Logger;
 import org.java_websocket.WebSocket;
 
 public class C2SGetLyricsPacket extends AbstractPacket implements C2SPacket {
@@ -15,12 +19,17 @@ public class C2SGetLyricsPacket extends AbstractPacket implements C2SPacket {
 
     @Override
     public void recivePacket(WebSocket socket) {
-        LyricsGetterManager.getLyricsGetterForText(track.trackInfo().title())
-                        .getLyrics(track.trackInfo().title() + " " + track.trackInfo().author()).thenAccept(lyrics -> {
-                    PacketHandler.sendPacket(socket, new S2CGetLyricsPacket(track, lyrics));
+        try {
+            LyricsGetterManager.getLyricsGetterForText(track.trackInfo().title())
+                            .getLyrics(track.trackInfo().title() + " " + track.trackInfo().author()).thenAccept(lyrics -> {
+                        PacketHandler.sendPacket(socket, new S2CGetLyricsPacket(track, lyrics));
 
 
-                });
+                    });
+        } catch (LyricsNotFoundException e) {
+            PacketHandler.sendPacket(socket, new S2CErrorPacket(getOpcode(),"Could not find lyrics"));
+            Logger.err(e.getMessage());
+        }
 
 
     }
